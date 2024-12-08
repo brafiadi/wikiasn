dd5361f412b2efa6c70e3c4f7f936793e4a4290a
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2024-12-09 03:51:49 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
+});
+const geistMono = localFont({
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
+});
+
+export const metadata: Metadata = {
+	title: "WikiASN",
+	description: "Ensiklopedia Aparatur Sipil Negara",
+	icons: {
+		icon: '/favicon-96x96.png'
+	}
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
