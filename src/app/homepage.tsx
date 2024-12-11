333de0b3ebd88c720987468b0d54b7769b36652d
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2024-12-11 10:20:44 +0700

    update

--- src/app/homepage.tsx
+++ src/app/homepage.tsx
@@ -5,7 +5,9 @@ import { Input } from "@/components/ui/input";
 import { Card } from "@/components/ui/card";
 import { CircleUserRound, Search } from "lucide-react";
 
-export default function WikiAsnHomepage() {
+export default async function WikiAsnHomepage() {
+	const data = await fetch('https://app.brafiadi.space/api/wikiasn/master-data/menu')
+	console.log(data)
 	return (
 		<div className="min-h-screen bg-gray-50/50 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-100 via-gray-50 to-white">
 			<div className="container mx-auto px-4">
@@ -69,15 +71,12 @@ export default function WikiAsnHomepage() {
 							<Card className="w-full max-w-2xl p-4 mt-8">
 								<div className="flex gap-4">
 									<div>
-										<h2 className="font-medium mb-2">
-											Hari Libur
-										</h2>
+										<h2 className="font-medium mb-2">Hari Libur</h2>
 										<p className="text-sm text-gray-600 mb-2">
 											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 											Sed diam magna, efficitur in felis sed, eleifend tempor
 											velit sed diam...
 										</p>
-										
 									</div>
 								</div>
 							</Card>
@@ -95,7 +94,6 @@ export default function WikiAsnHomepage() {
 											Sed diam magna, efficitur in felis sed, eleifend tempor
 											velit sed diam...
 										</p>
-										
 									</div>
 								</div>
 							</Card>
