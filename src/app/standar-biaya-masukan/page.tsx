2afc986c21edfb8e2232dd3197a0f9d2c5724e93
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2024-12-20 10:22:09 +0700

    add standar biaya masukan concept

--- src/app/standar-biaya-masukan/page.tsx
+++ src/app/standar-biaya-masukan/page.tsx
@@ -1,3 +1,11 @@
 export default function StandarBiayaMasukanPage() {
-	return <div>Dalam pengembangan</div>;
+	return <>
+	<div className="mb-8 rounded-lg bg-white p-4 m-4">
+				<h1 className="text-center text-xl font-bold text-neutral-700 md:text-2xl">
+					STANDAR BIAYA MASUKAN
+				</h1>
+			</div>
+
+			<div className="m-4">table</div>
+	</>;
 }
