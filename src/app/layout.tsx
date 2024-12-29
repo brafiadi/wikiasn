851e9b4560175d819206868ec8066d535d43bd7a
brafiadi <ahdiatgnr@gmail.com>
2024-12-29 15:37:16 +0700

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,20 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-1SM9R762R2"
+				/>
+				<script id="google-analytics">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-1SM9R762R2');
+					`}
+				</script>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
