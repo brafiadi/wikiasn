4e88ca8fd17f6e22ad0767fc0449522c6dfbf4d7
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2025-01-03 20:49:06 +0700

    minor update hari ini page

--- src/app/hari-ini/page.tsx
+++ src/app/hari-ini/page.tsx
@@ -1,3 +1,6 @@
+import Loader from "@/components/loader";
+import { Suspense } from "react";
+
 const apiUrl = process.env.API_URL;
 
 const getHariIniData = async () => {
@@ -6,11 +9,13 @@ const getHariIniData = async () => {
 	return data.data;
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
 	const data = await getHariIniData();
 	const isWork = !data.libur;
 	return (
-		<>
+		<Suspense fallback={<Loading />}>
 			<h2 className="text-xl text-gray-600 text-center">{data.hari_ini}</h2>
 			<h1
 				className={`text-5xl ${isWork ? "text-orange-600" : "text-red-600"} text-center`}
@@ -18,6 +23,14 @@ export default async function Page() {
 				{isWork ? "Hari Kerja" : `Libur ${data.hari_libur}`}
 			</h1>
 			<h2 className="text-xl text-gray-600 text-center">{data.pesan}</h2>
-		</>
+		</Suspense>
+	);
+}
+
+function Loading() {
+	return (
+		<div className="mx-auto min-h-[70vh] max-w-6xl p-4 md:p-8">
+			<Loader />
+		</div>
 	);
 }
