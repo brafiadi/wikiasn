bb0ca401dac7536b8967d45827c96a0a6f485918
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2025-02-05 11:47:42 +0700

    add peraturan page

--- src/app/peraturan/daftar-peraturan.table.tsx
+++ src/app/peraturan/daftar-peraturan.table.tsx
@@ -1,4 +1,14 @@
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { FileText } from "lucide-react";
+import Link from "next/link";
 
 const apiUrl = process.env.API_URL;
 
@@ -8,9 +18,17 @@ const getListDaftarPeraturanData = async () => {
 	return data.data;
 };
 
+interface Peraturan {
+	id: number;
+	nama: string;
+	tahun: string;
+	tautan: string;
+	kata_kunci: string;
+}
+
 export default async function DaftarPeraturanTable() {
 	const data = await getListDaftarPeraturanData();
-	console.log(data);
+	// console.log(data);
 	return (
 		<div className="min-h-[400px] rounded-lg bg-white py-8">
 			<Table>
@@ -18,10 +36,37 @@ export default async function DaftarPeraturanTable() {
 					<TableRow>
 						<TableHead className="w-[50px]">#</TableHead>
 						<TableHead>Judul Peraturan</TableHead>
-						<TableHead>Kategori</TableHead>
+						<TableHead>Tahun</TableHead>
+						<TableHead className="w-[200px]">Kategori</TableHead>
 						<TableHead>Dokumen</TableHead>
 					</TableRow>
 				</TableHeader>
+				<TableBody>
+					{data.map((item: Peraturan, index: number) => (
+						<TableRow key={item.id}>
+							<TableCell className="text-gray-500">{index + 1}</TableCell>
+							<TableCell>{item.nama}</TableCell>
+							<TableCell>{item.tahun}</TableCell>
+							<TableCell>
+								{item.kata_kunci.map((kataKunci: string, index: number) => (
+									<Badge
+										key={index}
+										className="bg-red-600/50 font-extralight mr-2 mb-2 capitalize"
+									>
+										{kataKunci}
+									</Badge>
+								))}
+							</TableCell>
+							<TableCell>
+								<Link href={item.tautan} target="_blank">
+									<Badge variant={"secondary"}>
+										<FileText className="text-gray-600 hover:text-gray-800 h-4 w-4" />
+									</Badge>
+								</Link>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
 			</Table>
 		</div>
 	);
