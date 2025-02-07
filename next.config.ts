9223093e68a55d408240022129e7b117256e96d9
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2025-02-07 08:46:36 +0700

    add react pdf for pdf viewer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.(pdf)$/,
+			type: "asset/resource",
+		});
+
+		return config;
+	},
 };
 
 export default nextConfig;
